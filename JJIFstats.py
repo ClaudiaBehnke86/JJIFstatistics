1a4182a0b826033379426280dd9b40838d3668bd
Claudia Behnke <claudia.behnke@online.de>
2022-05-29 09:30:55 +0200

    fix progress bar

--- JJIFstats.py
+++ JJIFstats.py
@@ -17,6 +17,13 @@ import plotly.graph_objs as pg
 import numpy as np
 import os
 
+
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import csr_matrix
+import sparse_dot_topn.sparse_dot_topn as ct  # Leading Juice for us
+import time
+
 # the supported age_divisions
 AGE_INP = ["U16", "U18", "U21", "Adults", "U14", "U12", "U10", "U15"]
 AGE_SEL = ["U16", "U18", "U21", "Adults"]  # preselected age_divisions
@@ -176,6 +183,10 @@ def get_events(dstart, dend, evtt_select, user, password):
               'name': 'Asian Championship 2016',
               'eventtype': 'Continental Championship',
               'startDate': '2016-12-09'},
+              {'id': 'YWCH2017', 'country_code': 'GRE',
+              'name': 'Youth World Championships 2017 Athens',
+              'eventtype': 'World Championship',
+              'startDate': '2017-03-17'},
                 ]
     df2 = df2.append(df_wg, ignore_index = True)            
 
@@ -218,7 +229,7 @@ def update_events(df_evts, age_select, dis_select, cont_select, evtt):
                     df_file = df_file[['category_id', 'category_name', 'country_code', 'rank', 'name']]
                     df_file['id'] = val
                     frames.append(df_file)
-                my_bar.progress(i)
+                my_bar.progress((i+1)/len(events))
 
     return frames
 
@@ -259,6 +270,66 @@ def conv_to_type(df, type_name, type_list):
         df[type_name].where(~(df[type_name].str.contains(x)),
                             other=x, inplace=True)
     return df[type_name]
+
+
+def ngrams(string, n=3):
+    ''' used to check for simkliar names'''
+    string = re.sub(r'[,-./]|\sBD',r'', string)
+    ngrams = zip(*[string[i:] for i in range(n)])
+    return [''.join(ngram) for ngram in ngrams]
+
+def awesome_cossim_top(A, B, ntop, lower_bound=0):
+    # force A and B as a CSR matrix.
+    # If they have already been CSR, there is no overhead
+    A = A.tocsr()
+    B = B.tocsr()
+    M, _ = A.shape
+    _, N = B.shape
+ 
+    idx_dtype = np.int32
+ 
+    nnz_max = M*ntop
+ 
+    indptr = np.zeros(M+1, dtype=idx_dtype)
+    indices = np.zeros(nnz_max, dtype=idx_dtype)
+    data = np.zeros(nnz_max, dtype=A.dtype)
+    ct.sparse_dot_topn(
+        M, N, np.asarray(A.indptr, dtype=idx_dtype),
+        np.asarray(A.indices, dtype=idx_dtype),
+        A.data,
+        np.asarray(B.indptr, dtype=idx_dtype),
+        np.asarray(B.indices, dtype=idx_dtype),
+        B.data,
+        ntop,
+        lower_bound,
+        indptr, indices, data)
+    return csr_matrix((data,indices,indptr),shape=(M,N))    
+
+# unpacks the resulting sparse matrix
+def get_matches_df(sparse_matrix, name_vector, top=100):
+    non_zeros = sparse_matrix.nonzero()
+    
+    sparserows = non_zeros[0]
+    sparsecols = non_zeros[1]
+    
+    if top:
+        nr_matches = top
+    else:
+        nr_matches = sparsecols.size
+    
+    left_side = np.empty([nr_matches], dtype=object)
+    right_side = np.empty([nr_matches], dtype=object)
+    similairity = np.zeros(nr_matches)
+    
+    for index in range(0, nr_matches):
+        left_side[index] = name_vector[sparserows[index]]
+        right_side[index] = name_vector[sparsecols[index]]
+        similairity[index] = sparse_matrix.data[index]
+    
+    return pd.DataFrame({'left_side': left_side,
+                          'right_side': right_side,
+                           'similairity': similairity})
+
 
 IOC_ISO = read_in_iso()
 key_map = read_in_catkey()
@@ -424,6 +495,22 @@ else:
     df_time['continent'].where(~(df_time['continent'].str.contains("North America")),
                           other="Pan America", inplace=True)
     # start grapics here
+
+    # names_types = df_time['name']
+    # vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams)
+    # tf_idf_matrix = vectorizer.fit_transform(names_types) 
+    
+    # st.write(tf_idf_matrix[0])
+    # matches = awesome_cossim_top(tf_idf_matrix, tf_idf_matrix.transpose(), 10, 0.8)
+
+
+    # # store the  matches into new dataframe called matched_df and 
+    # # printing 10 samples
+    # matches_df = get_matches_df(matches, df_time['name'], top=200)
+    # matches_df = matches_df[matches_df['similairity'] < 0.99999] # For removing all exact matches
+    # matches_df.sample(10)
+    # # printing the matches in sorted order
+    # matches_df.sort_values(['similairity'], ascending=False).head(10)
 
     st.header('JJIF statistic')
 
