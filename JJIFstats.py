03e0a4eb1739db00f5901b933db20c6aef38a93e
Claudia Behnke <claudia.behnke@online.de>
2023-05-07 12:28:01 +0200

    replace append df

--- JJIFstats.py
+++ JJIFstats.py
@@ -173,7 +173,9 @@ def get_events(dstart_in, dend_in, evtt_select, user, password):
         ['id', 'country_code', 'name', 'eventtype', 'startDate']
     ]
 
-    df2 = df2.append(df_wg, ignore_index=True)
+    # df2 = df2.append(df_wg, ignore_index=True)
+    df2 = pd.concat([df2, df_wg])
+
     df2['startDate'] = pd.to_datetime(df2["startDate"]).dt.date
     df2 = df2[df2['startDate'].between(dstart_in, dend_in, inclusive=False)]
     df2 = df2[df2['eventtype'].isin(evtt_select)]
@@ -508,6 +510,7 @@ else:
             df_new.loc[:, 'name'] = df_new['name'].replace(dict_map)
 
             list_df_new.append(df_new)
+
             # if len(dict_map) > 0:
             #    print('fixing ' + str(len(dict_map)) + ' issues with names')
 
